b7fa805e55460ab5ef46fe1ad958bb7f872b0b49
JK-mern <Jkmerndev@gmail.com>
2023-12-16 20:04:32 +0530

    completed signup functionality

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,19 +14,18 @@ mongoose
     console.log(err);
   });
 
-
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
 
-app.use('/api/user',userRouter)
-app.use('/api/auth', authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-app.use((err,req,res,next) =>{
-  const statusCode = err.statusCode || 500
-  const message = err.message || "Internal Server Error "
-  return res.status(statusCode).json({success : false, statusCode, message })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error ";
+  return res.status(statusCode).json({ success: false, statusCode, message });
+});
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,31 +1,73 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [err,setError] = useState(null)
+  const [loading,setLoading] = useState(false)
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true)
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    if(data.success === false){
+      setError(data.message)
+      setLoading(false)
+      return
+    }
+      setLoading(false)
+      setError(null)
+      navigate('/sign-in')
+      
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
+    
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-center text-3xl font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="rounded-lg p-3 focus:outline-slate-700"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Email"
           id="email"
           className="rounded-lg p-3  focus:outline-slate-700"
+          onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="Password"
           id="password"
           className="rounded-lg p-3  focus:outline-slate-700"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 p-3  rounded-lg  text-white hover:opacity-95 disabled:opacity-80">
-          SIGN UP
+        <button disabled={loading} className="bg-slate-700 p-3  rounded-lg  text-white hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading' : 'SIGN UP'}
         </button>
         <button className="bg-red-700 p-3  rounded-lg  text-white">
           CONTINUE WITH GOOGLE
@@ -33,10 +75,11 @@ function SignUp() {
       </form>
       <div className="flex gap-2 mt-5 ">
         <p>Have an account ?</p>
-        <Link to={"/sign-in"}> 
-        <span className="text-blue-500">Sign in</span>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      {err && <p className= 'text-red-800  mt-3 text-base font-medium'>{err}</p>}
     </div>
   );
 }
