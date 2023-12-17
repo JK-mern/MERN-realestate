6253d15c43d91023275af0bdc535fea1839264a5
JK-mern <Jkmerndev@gmail.com>
2023-12-17 20:40:43 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 12);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +11,26 @@ export const signup = async (req, res,next) => {
     await newUser.save();
     res.status(201).json("User created Successfully");
   } catch (error) {
-    next(error)
+    next(error); //router will go to the middleware having four paramters (error handling middleware)
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    console.log(validUser);
+    if (!validUser) {
+      return next(errorHandler(404, "User not Found!"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials!"));
+    }
+    const {password:pass , ...rest} = validUser._doc
+    const token = jwt.sign({id : validUser.id}, process.env.JWT_SECRET)
+    res.cookie('accessToken',token,{httpOnly : true , maxAge: 1000*60*60*24*30}).status(200).json(rest)
+  } catch (error) {
+    next(error);
   }
 };
--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SignIn() {
+  const [formData,setFormData] = useState({})
+  const [loading,setLoading] = useState(false)
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  }
+  const handleSubmit = (e) =>[
+    e.preventDefault()
+  ]
+console.log(formData)
   return (
-    <div>
-      SignIn
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center text-3xl font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          className="rounded-lg p-3  focus:outline-slate-700"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="rounded-lg p-3  focus:outline-slate-700"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 p-3  rounded-lg  text-white hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading" : "SIGN UP"}
+        </button>
+        <button className="bg-red-700 p-3  rounded-lg  text-white">
+          CONTINUE WITH GOOGLE
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 ">
+        <p>Don't Have an account ?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-500">Sign in</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
