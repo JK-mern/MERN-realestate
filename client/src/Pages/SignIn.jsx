d2ee9b2a976d3d384e00f96055f0f4543d11071f
JK-mern <Jkmerndev@gmail.com>
2023-12-17 20:52:09 +0530

    completed sign in functionality

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,23 +1,50 @@
 
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignIn() {
   const [formData,setFormData] = useState({})
   const [loading,setLoading] = useState(false)
+  const [err,setError] = useState(null)
+  const navigate = useNavigate()
   const handleChange = (e) =>{
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     })
   }
-  const handleSubmit = (e) =>[
+  const handleSubmit = async(e) =>{
     e.preventDefault()
-  ]
+    try {
+      setLoading(true)
+    const res = await fetch("/api/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    if(data.success === false){
+      setError(data.message)
+      setLoading(false)
+      return
+    }
+      setLoading(false)
+      setError(null)
+      navigate('/')
+      
+    } catch (error) {
+       setLoading(false)
+      
+       setError(error.message)
+     }
+}
 console.log(formData)
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-center text-3xl font-semibold my-7">Sign Up</h1>
+      <h1 className="text-center text-3xl font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -37,7 +64,7 @@ console.log(formData)
           disabled={loading}
           className="bg-slate-700 p-3  rounded-lg  text-white hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading" : "SIGN UP"}
+          {loading ? "Loading" : "SIGN IN"}
         </button>
         <button className="bg-red-700 p-3  rounded-lg  text-white">
           CONTINUE WITH GOOGLE
@@ -46,9 +73,10 @@ console.log(formData)
       <div className="flex gap-2 mt-5 ">
         <p>Don't Have an account ?</p>
         <Link to={"/sign-up"}>
-          <span className="text-blue-500">Sign in</span>
+          <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
+      {err && <p className= 'text-red-800  mt-3 text-base font-medium'>{err}</p>}
     </div>
   );
 }
